1a69b16b41dcea6d18eed542f5bac8616472a2cb
Arcticweasel <Arcticweasel@users.noreply.github.com>
2019-10-14 15:48:31 +0200

    Add Top 10 countries 2100 in population

--- 2_explore.R
+++ 2_explore.R
@@ -26,6 +26,11 @@ p1 <- df %>%
 class(df)
 
 # Top 10 countries in 2100 wrt total population
+p2 <- df %>%
+    filter(year=="2100") %>%
+    group_by(country) %>%
+    summarise(population=sum(population)) %>%
+    arrange(desc(population))
 
 # Top 10 countries with respect to the change in total population between 1950 and 2100
 
